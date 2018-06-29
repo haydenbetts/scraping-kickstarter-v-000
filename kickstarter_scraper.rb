51f702b515da6290e5ae8c4ee650ffb77dc55daf
Hayden Betts <haydenbetts@gmail.com>
2018-06-29 16:42:36 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,11 +1,13 @@
 # require libraries/modules here
+
+# scaping steps: get, make, print
 require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  
+
 end
 
 def 
