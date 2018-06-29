3205f9570a72d4bba236b09b467957dde80a33ae
Hayden Betts <haydenbetts@gmail.com>
2018-06-29 16:45:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,10 +8,12 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
+  kickstarter.css(".project-card")
+
 end
 
 def get_project_name(xml_nodes)
-  
+  xml_nodes
 end
 
 def get_project_props
