44dea04bfb1587542d8382964cbb964bc357b974
Hayden Betts <haydenbetts@gmail.com>
2018-06-29 17:08:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,15 +10,13 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   project_nodes = kickstarter.css(".project-card")
-  projects = project_nodes.collect do |project|
-    {
-      project.css(".bbcard_name").css("a").text => {
+  project_nodes.each do |project|
+    :projects[project.css(".bbcard_name").css("a").text] => {
         :image_link => project.css(".project-thumbnail img").attr("src").value,
         :description => project.css(".bbcard_blurb").text,
         :location => project.css(".location-name").text,
         :percent_funded => project.css(".first.funded strong").text
       }
-    }
   end
 
 
